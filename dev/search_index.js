19435f2e287ab8dd453a5e8a31d139c1115ad896
Documenter.jl <documenter@juliadocs.github.io>
2024-12-29 17:54:34 +0000

    build based on 6f50d34

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TimeSeriesFunk","category":"page"},{"location":"#TimeSeriesFunk","page":"Home","title":"TimeSeriesFunk","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TimeSeriesFunk.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TimeSeriesFunk]","category":"page"}]
+}
